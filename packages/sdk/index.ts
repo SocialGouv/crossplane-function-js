317648d97b9b34d59798fcd851ca20ace24784e9
devthejo <devthejo@protonmail.com>
2025-07-31 14:07:58 +0200

    fix: wip

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -4,6 +4,6 @@ export { logger, createLogger } from "@crossplane-js/libs"
 // Export types
 export type * from "./src/types"
 
-export * from "./src/Model"
+export * from "./src/Model/index"
 export * from "./src/utils/FieldRef"
 export * from "./src/utils/secretUtils"
